6a1cf8c608fc31bd424e03af26619549ab194b18
TheSagnikKing <sagniknandy27@gmail.com>
2024-04-03 23:48:10 +0530

    commit

--- src/components/SidebarAnimation.jsx
+++ src/components/SidebarAnimation.jsx
@@ -3,18 +3,35 @@ import './SidebarAnimation.css'
 
 const SidebarAnimation = () => {
 
-    const [showSidebar, setShowSidebar] = useState(true)
-    return (
-        <div className='sidebar_container'>
-            <div className={`${showSidebar ? "sidebarshow" : "sidebarhide"}`}>
-                <h1 className={`${showSidebar ? "menushow" : "menuhide"}`}>Menus</h1>
-            </div>
+  const [open, setOpen] = useState(false)
+
+  const [leftbarOpen, setLeftBarOpen] = useState(false)
+
+  return (
+    <main className="sidebar_container_main">
+      <div className='sidebar_header'>
+        <div className='left_btn' onClick={() => setLeftBarOpen((prev) => !prev)}>Open/Close</div>
+        <button onClick={() => setOpen((prev) => !prev)}>Toggle</button>
+      </div>
+ 
+      <div className={`sidebar_dropdownmenu ${open ? 'active' : 'inactive'}`}>
+          <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Dashboard</h5>
+          <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Salon List</h5>
+          <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Queue List</h5>
+          <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Appointments</h5>
+      </div>
 
 
-            <button onClick={() => setShowSidebar((prev) => !prev)}>open/close</button>
-        </div>
-    )
+      <main className={`leftbar ${leftbarOpen ? 'leftbar_active' : 'leftbar_inactive'}`}>
+        <p>Dashboard</p>      
+        <p>Salon List</p>
+        <p>QueueList</p>
+        <p>Appointments</p>
+      </main>
+
+      
+    </main>
+  )
 }
 
 export default SidebarAnimation
-
