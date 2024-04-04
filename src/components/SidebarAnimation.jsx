90372917dcc193ed0568ebdc7f64a950e985f989
TheSagnikKing <sagniknandy27@gmail.com>
2024-04-04 11:18:21 +0530

    commit

--- src/components/SidebarAnimation.jsx
+++ src/components/SidebarAnimation.jsx
@@ -1,24 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './SidebarAnimation.css'
 
 const SidebarAnimation = () => {
 
   const [open, setOpen] = useState(false)
 
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    // Function to handle clicks outside the dropdown menu
+    const handleClickOutside = (event) => {
+      if (!dropdownRef.current.contains(event.target)) {
+        setOpen(false); // Close the dropdown if clicked outside
+      }
+      console.log(dropdownRef.current.contains(event.target))
+    };
+
+    // Add event listener to detect clicks outside the dropdown menu
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      // Cleanup: remove event listener when component unmounts
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [setOpen]);
+
   const [leftbarOpen, setLeftBarOpen] = useState(false)
 
   return (
-    <main className="sidebar_container_main">
+    <main className="sidebar_container_main" >
       <div className='sidebar_header'>
         <div className='left_btn' onClick={() => setLeftBarOpen((prev) => !prev)}>Open/Close</div>
         <button onClick={() => setOpen((prev) => !prev)}>Toggle</button>
       </div>
- 
-      <div className={`sidebar_dropdownmenu ${open ? 'active' : 'inactive'}`}>
-          <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Dashboard</h5>
-          <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Salon List</h5>
-          <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Queue List</h5>
-          <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Appointments</h5>
+
+      <div className={`sidebar_dropdownmenu ${open ? 'active' : 'inactive'}`} ref={dropdownRef}>
+        <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Dashboard</h5>
+        <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Salon List</h5>
+        <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Queue List</h5>
+        <h5 className={`sidebar_text ${open ? 'textactive' : 'textinactive'}`}>Appointments</h5>
       </div>
 
 
@@ -29,7 +49,6 @@ const SidebarAnimation = () => {
         <p>Appointments</p>
       </main>
 
-      
     </main>
   )
 }
