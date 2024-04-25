0c64cefbe847726a54c5c575ff7c3f662f2b417d
TheSagnikKing <sagniknandy27@gmail.com>
2024-04-25 12:57:00 +0530

    commit datagrid 2

--- src/components/Datagrid2.jsx
+++ src/components/Datagrid2.jsx
@@ -1,79 +1,3 @@
-// import React from 'react'
-// import Box from '@mui/material/Box';
-// import { DataGrid } from '@mui/x-data-grid';
-
-// const columns = [
-//     { field: 'id', headerName: 'ID', width: 90 },
-//     {
-//         field: 'firstName',
-//         headerName: 'First name',
-//         width: 150,
-//         editable: true,
-//     },
-//     {
-//         field: 'lastName',
-//         headerName: 'Last name',
-//         width: 150,
-//         editable: true,
-//     },
-//     {
-//         field: 'age',
-//         headerName: 'Age',
-//         type: 'number',
-//         width: 110,
-//         editable: true,
-//     },
-//     {
-//         field: 'fullName',
-//         headerName: 'Full name',
-//         description: 'This column has a value getter and is not sortable.',
-//         sortable: false,
-//         width: 160,
-//         valueGetter: (params) =>
-//             `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-//     },
-// ];
-
-// const rows = [
-//     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-//     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-//     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-//     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-//     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-//     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-//     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-//     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-//     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-// ];
-
-
-
-// const Datagrid2 = () => {
-//     return (
-//         <Box sx={{ height: 400, width: '80%',margin:"auto",marginTop: "30px" }}>
-//             <DataGrid
-//                 rows={rows}
-//                 columns={columns}
-//                 // initialState={{
-//                 //     pagination: {
-//                 //         paginationModel: {
-//                 //             pageSize: 5,
-//                 //         },
-//                 //     },
-//                 // }}
-//                 pageSizeOptions={[5]}
-//                 checkboxSelection
-//                 disableRowSelectionOnClick
-//                 // hideFooterPagination
-//                 // hideFooter 
-//             />
-
-//         </Box>
-//     )
-// }
-
-// export default Datagrid2
-
 import React, { useState } from 'react';
 import "./Datagrid2.css"
 import Box from '@mui/material/Box';
@@ -141,7 +65,6 @@ const Datagrid2 = () => {
     const [page, setPage] = useState(1);
     const pageSize = rowsPerPage;
     const totalRows = rows.length;
-    // console.log(totalRows)
     const totalPages = Math.ceil(totalRows / pageSize);
 
     const handlePageChange = (event, value) => {
@@ -152,9 +75,7 @@ const Datagrid2 = () => {
     const paginatedRows = rows.slice((page - 1) * pageSize, page * pageSize);
 
     const startIndex = (page - 1) * pageSize
-    // console.log("startIndex ", startIndex)
     const endIndex = Math.min(page * pageSize,totalRows)
-    // console.log("endIndex ", endIndex)
 
     console.log("start - end Index ", startIndex, endIndex)
 
@@ -167,6 +88,7 @@ const Datagrid2 = () => {
                 checkboxSelection={false}
                 disableRowSelectionOnClick
                 hideFooterPagination
+                disableColumnMenu={true} 
                 hideFooter
             />
 
